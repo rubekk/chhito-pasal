5d4bdba0a4d2238a0fc528ed480b87375c96e22f
Rubek Maharjan <rubekmhzn7@gmail.com>
2024-10-27 17:46:30 +0545

    -

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -4,13 +4,22 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA48xm1MjqdslMQNJ8KC7t8cyQxb7_JVM4",
+//     authDomain: "onlineshop-48c01.firebaseapp.com",
+//     projectId: "onlineshop-48c01",
+//     storageBucket: "onlineshop-48c01.appspot.com",
+//     messagingSenderId: "556297026777",
+//     appId: "1:556297026777:web:d77af098bbdf00420192c8"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA48xm1MjqdslMQNJ8KC7t8cyQxb7_JVM4",
-    authDomain: "onlineshop-48c01.firebaseapp.com",
-    projectId: "onlineshop-48c01",
-    storageBucket: "onlineshop-48c01.appspot.com",
-    messagingSenderId: "556297026777",
-    appId: "1:556297026777:web:d77af098bbdf00420192c8"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
